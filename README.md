36d37f742e2f14555ab9ed9fd0e521139cb63fe7
vivianliang <vliang88@gmail.com>
2014-11-15 18:46:16 -0600

    add ability to read from stdin

--- README.md
+++ README.md
@@ -3,9 +3,9 @@ humane-sort
 
 a more humane sort
 
-Sorts string characters according to ASCII value unless
-digit substrings are in question, in which case
-the numbers will be evaluated numerically.
+Sorts a list of strings from file or stdin according to ASCII value unless
+digit substrings are in question, in which case the numbers will be evaluated
+numerically.
 
 	a1.txt
 	a2.txt
@@ -13,6 +13,27 @@ the numbers will be evaluated numerically.
 	a10.txt
 	a12.txt
 
+To compile:
+
+	make all
+
+Read list of strings from file, sort, and print sorted list to stdout.
+Example:
+	./humane_sort file.txt
+
+Read list of strings from stdin, sort, and print sorted list to stdout.
+Examples:
+	./humane_sort < file.txt
+	cat file.txt | ./humane_sort
+	ls | ./humane_sort
+
+Run development tests:
+
+	./test
+
+---
+Some implementation notes:
+	
 This implementation will place all Uppercase letters
 before Lowercase letters.
 
@@ -35,16 +56,3 @@ leading zeroes will have priority.
 
 	001.txt
 	1.txt
-
-To compile:
-
-	make all
-
-Sort a text file (to stdout):
-
-	./humane_sort file.txt
-
-Run tests
-
-	./test
-	
--- main.c
+++ main.c
@@ -5,10 +5,16 @@
 #define MAX_LINE_LENGTH	50
 #define MAX_NUM_LINES	100
 
-// Usage: ./humane_sort file.txt
+/* 
+ * Usage: humane_sort file.txt
+ *        humane_sort < file.txt
+ *        cat file.txt | humane_sort
+ *        ls | humane_sort
+ */
 int main(int argc, char *argv[]){
 	int i = 0;
-	int j = 0;	
+	int j = 0;
+	int read_from_file = 0;
 	char** str_list = NULL;
 
 	// Allocate space for strings
@@ -21,26 +27,35 @@ int main(int argc, char *argv[]){
 		str_list[j] = (char*)malloc(sizeof(char)*(MAX_LINE_LENGTH+1));
 	}
 	
-	// Read strings file
-	FILE * fp;
-	fp = fopen(argv[1],"r");
-	if (fp == NULL){
-		fprintf(stderr,"error opening file");
-		fclose(fp);
-		exit(1);
-	}
-	while (i < MAX_NUM_LINES){
-		if (fscanf(fp, "%s", str_list[i])==EOF){
-			break;
+	// If a filename is specified, read strings file
+	if (argv[1]){
+		FILE * fp;
+		fp = fopen(argv[1],"r");
+		if (fp == NULL){
+			fprintf(stderr,"error opening file");
+			fclose(fp);
+			exit(1);
 		}
-		i++;
+		while (i < MAX_NUM_LINES){
+			if (fscanf(fp, "%s", str_list[i]) == EOF){
+				break;
+			}
+			i++;
+		}
+		fclose(fp);
+	// Otherwise, read strings from stdin
+	} else {
+		while (i < MAX_NUM_LINES){
+			if (scanf("%s", str_list[i]) == EOF){
+				break;
+			}
+			i++;
+		}
 	}
 
 	humane_sort((const char **)str_list, i);
 
 	print_strings((const char **)str_list,i);
-
-	fclose(fp);
 
 	// Free strings
 	for (j = 0;j<MAX_NUM_LINES;j++){
